12e0de8642fd353114028a8c5d20bab8757e855b
Fajar Pambudi <162731923+Rafaaaaa27@users.noreply.github.com>
2026-07-08 13:59:11 +0700

    summary

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-php -r "copy('https://getcomposer.org/installer', '/tmp/composer-setup.php');"
-php /tmp/composer-setup.php --install-dir=/tmp --filename=composer --quiet
-/tmp/composer install --no-dev --optimize-autoloader
-cp .env.example .env
-php artisan key:generate
